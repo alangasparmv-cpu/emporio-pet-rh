8f6b3045549084f4ae59f91d37dab16740da22f9
alangasparmv-cpu <alangasparmv@gmail.com>
2026-03-09 18:41:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'emporio-rh-v7-cache';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
